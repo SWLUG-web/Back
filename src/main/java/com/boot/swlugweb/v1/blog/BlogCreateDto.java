77b29cba17c58f1c1d46c8e3920b38d3ab3a9791
Lee-Song-Ha <songha1744@swu.ac.kr>
2025-01-11 01:20:20 +0900

    [song] blog 저장

---
+++ src/main/java/com/boot/swlugweb/v1/blog/BlogCreateDto.java
@@ -0,0 +1,23 @@
+package com.boot.swlugweb.v1.blog;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Setter
+@Getter
+@Builder
+public class BlogCreateDto {
+    private Integer boardCategory;
+    private String boardTitle;
+    private String boardContent;
+    private List<String> tag;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    private LocalDateTime createAt;
+    private String imageUrl;
+}
--- src/main/java/com/boot/swlugweb/v1/blog/BlogService.java
+++ src/main/java/com/boot/swlugweb/v1/blog/BlogService.java
@@ -1,19 +1,21 @@
 package com.boot.swlugweb.v1.blog;
 
-import com.boot.swlugweb.v1.main.MainRepository;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
 public class BlogService {
 
     private final BlogRepository blogRepository;
+    private final BlogDetailRepository blogDetailRepository;
 
-    public BlogService(BlogRepository blogRepository) {
+    public BlogService(BlogRepository blogRepository, BlogDetailRepository blogDetailRepository) {
         this.blogRepository = blogRepository;
+        this.blogDetailRepository = blogDetailRepository;
     }
 
     public List<BlogDto> getBlogs() {
@@ -23,5 +25,21 @@ public class BlogService {
     public List<BlogDto> getBlogs(int page) {
         Pageable pageable = PageRequest.of(page, 8);
         return blogRepository.findByBoardCategory(pageable);
+    }
+
+    public BlogDomain createBlog(BlogCreateDto blogCreateDto, String userId) {
+        BlogDomain blogDomain = new BlogDomain();
+
+        blogDomain.setUserId(userId);
+        blogDomain.setBoardCategory(blogCreateDto.getBoardCategory());
+        blogDomain.setBoardTitle(blogCreateDto.getBoardTitle());
+        blogDomain.setBoardContents(blogCreateDto.getBoardContent());
+        blogDomain.setCreateAt(blogCreateDto.getCreateAt() != null ? blogCreateDto.getCreateAt() : LocalDateTime.now());
+        blogDomain.setIsPin(false);
+        blogDomain.setIsSecure(false);
+        blogDomain.setIsDelete(false);
+
+        // MongoDB에 저장
+        return blogDetailRepository.save(blogDomain);
     }
 }
