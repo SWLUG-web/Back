1bca48d4b7be3d4b5a0978cb9a5d034a51bb2f79
mnzy412 <mnzy412@gmail.com>
2025-02-01 04:18:15 +0900

    [mnzy] 공지사항 글 넘버 제거

--- src/main/java/com/boot/swlugweb/v1/main/MainDomain.java
+++ src/main/java/com/boot/swlugweb/v1/main/MainDomain.java
@@ -7,30 +7,35 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Document(collection = "SwlugWebTest")
 @Getter
 @Setter
 public class MainDomain {
-
     @Id
     private String id;
 
     @Field("user_id")
     private String userId;
 
+    private String nickname;  // 닉네임 필드 추가
+
     @Field("board_title")
-    private String boardTitle;
+    private String noticeTitle;  // boardTitle에서 noticeTitle로 변경
 
     @Field("created_at")
     private LocalDateTime createdAt;
 
+    @Field("tag")
+    private List<String> tag;
+
     @Field("is_pin")
-    private boolean isPin;
+    private Boolean isPin = false;
 
     @Field("is_secure")
-    private int isSecure;
+    private Integer isSecure = 0;
 
     @Field("is_delete")
-    private int isDelete;
+    private Integer isDelete = 0;
 }
