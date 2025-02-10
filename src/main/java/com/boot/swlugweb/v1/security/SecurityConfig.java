a6b418d6b5532129275ca32320b58337f5246037
Lee-Song-Ha <songha1744@swu.ac.kr>
2025-02-11 00:08:18 +0900

    [song] error2

--- src/main/java/com/boot/swlugweb/v1/security/SecurityConfig.java
+++ src/main/java/com/boot/swlugweb/v1/security/SecurityConfig.java
@@ -3,7 +3,7 @@ package com.boot.swlugweb.v1.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -56,7 +56,7 @@ public class SecurityConfig {
                         .requestMatchers("/error").permitAll()
                         // Admin 페이지 관련 권한
                         .requestMatchers("/api/admin/**").permitAll()
-                        
+
                 );
         return http.build();
     }
